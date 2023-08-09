bb4993071025f3dd87ac617fa275ef12f3bf27b4
SangamSaiSantosh <105221214+SaiSantoshSangam@users.noreply.github.com>
2023-08-09 20:28:47 +0530

    .....

--- scr/Data/Products.js
+++ scr/Data/Products.js
@@ -2,7 +2,8 @@ const pro = Array.from([
   {
     _id: "1",
     name: "Nike for Mens (Blue & Orange) ",
-    image: "dfs",
+    image:
+      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pngwing.com%2Fen%2Fsearch%3Fq%3Dnike%2Bshoes&psig=AOvVaw2vH_yIY5Krj0M1aoFAxBUW&ust=1691678769486000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCKj8g-boz4ADFQAAAAAdAAAAABAE",
     description: "Brand New NIKE shoes for Mens ",
     price: "$99",
     countInStock: 5,
@@ -13,7 +14,8 @@ const pro = Array.from([
   {
     _id: "2",
     name: "Adidas for Mens (White) ",
-    image: URL("./2.jpeg"),
+    image:
+      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pngplay.com%2Fimage%2F97083&psig=AOvVaw0UJ7-T8PMr_-Dr-3X-AGGW&ust=1691678945476000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCICd8qXpz4ADFQAAAAAdAAAAABAE",
     description: "Brand New ADIDAS shoes for Mens ",
     price: "$89",
     countInStock: 5,
@@ -24,7 +26,8 @@ const pro = Array.from([
   {
     _id: "3",
     name: "Nike for Mens (yellow) ",
-    image: "./3.jpeg",
+    image:
+      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpngimg.com%2Fimage%2F5816&psig=AOvVaw1wBG_lbUykTi688462vjH3&ust=1691678986605000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCLDpwbnpz4ADFQAAAAAdAAAAABAE",
     description: "Brand New NIKE shoes for Mens ",
     price: "$99",
     countInStock: 5,
@@ -35,7 +38,8 @@ const pro = Array.from([
   {
     _id: "4",
     name: "Puma for Mens (White & Black) ",
-    image: "./4.jpeg",
+    image:
+      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pngwing.com%2Fen%2Ffree-png-zkeli&psig=AOvVaw0uvUM6dDCMlkdOX5Toyu3y&ust=1691679015693000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCMiCjcfpz4ADFQAAAAAdAAAAABAE",
     description: "Brand New PUMA shoes for Mens ",
     price: "$79",
     countInStock: 4,
@@ -46,7 +50,8 @@ const pro = Array.from([
   {
     _id: "5",
     name: "WoodLand for Mens  ",
-    image: "./4.jpeg",
+    image:
+      "https://www.google.com/imgres?imgurl=https%3A%2F%2Fpngfile.net%2Fpublic%2Fuploads%2Fpreview%2Fwoodland-camel-original-shoe-png-free-download-21566294570chp7r0mguf.png&tbnid=e_x2THsOEGJn3M&vet=12ahUKEwiO7eDY6c-AAxVN6KACHd-BBpUQMygBegUIARDZAQ..i&imgrefurl=https%3A%2F%2Fpngfile.net%2Fphoto%2F125%2Fwoodland-camel-original-shoe-png-free-download&docid=y_KlJlZ2QlN78M&w=850&h=756&q=WoodLand%20shoes%20without%20background&ved=2ahUKEwiO7eDY6c-AAxVN6KACHd-BBpUQMygBegUIARDZAQ",
     description: "Brand New WOODLAND shoes for Mens ",
     price: "$149",
     countInStock: 8,
@@ -57,7 +62,8 @@ const pro = Array.from([
   {
     _id: "6",
     name: "Bata for Mens ( sky-blue) ",
-    image: "./4.jpeg",
+    image:
+      "https://www.google.com/imgres?imgurl=https%3A%2F%2Ficon2.cleanpng.com%2F20180403%2Fcgw%2Fkisspng-steel-toe-boot-sneakers-bata-shoes-nike-sneakers-5ac362708b2288.7329786215227541605699.jpg&tbnid=5EUmMmS6edneFM&vet=12ahUKEwjIiJX56c-AAxVrmmMGHe6DDekQMygGegUIARDZAQ..i&imgrefurl=https%3A%2F%2Fwww.cleanpng.com%2Ffree%2Fbata-shoes.html&docid=FyxAh7Lfwacl0M&w=260&h=260&q=bata%20shoes%20without%20background&ved=2ahUKEwjIiJX56c-AAxVrmmMGHe6DDekQMygGegUIARDZAQ",
     description: "Brand New BATA shoes for Mens ",
     price: "$59",
     countInStock: 10,

d28743859d10803ae0a2a9bb3f21af549d5b7ddd
SangamSaiSantosh <105221214+SaiSantoshSangam@users.noreply.github.com>
2023-08-09 20:15:31 +0530

    ...

--- App.js
+++ App.js
@@ -1,14 +1,11 @@
 import React from "react";
-import {Box, Text, Input,button} from 'native-base'
+import { Box, Text, Input, button } from "native-base";
 
-import {
-  NativeBaseProvider,
-   
-} from "native-base";
+import { NativeBaseProvider } from "native-base";
 import NativeBaseIcon from "./components/NativeBaseIcon";
 import { Platform } from "react-native";
- import LoginScreen from "./scr/Screens/LoginScreen";
- import HomeScreen from "./scr/Screens/HomeScreen"
+import LoginScreen from "./scr/Screens/LoginScreen";
+import HomeScreen from "./scr/Screens/HomeScreen";
 import RegisterScreen from "./scr/Screens/RegisterScreen";
 import NotVerfiyScreen from "./scr/Screens/NotVerfiyScreen";
 
@@ -19,15 +16,9 @@ const config = {
 };
 
 export default function App() {
-  const [image,setimage] =  useState("");
   return (
-   <NativeBaseProvider>
-         <HomeScreen/> 
-    
-  </NativeBaseProvider>
-     
-   
+    <NativeBaseProvider>
+      <HomeScreen />
+    </NativeBaseProvider>
   );
 }
-
-
--- scr/Data/Products.js
+++ scr/Data/Products.js
@@ -1,76 +1,68 @@
- const pro=Array.from([
-        
-    {
-        _id : "1",
-        name :"Nike for Mens (Blue & Orange) ",
-        image:"./1.jpeg",
-        description:"Brand New NIKE shoes for Mens ",
-        price:"$99",
-        countInStock:5, 
-        rating:4.5/5,
-        numReviews:20,
-    },
+const pro = Array.from([
+  {
+    _id: "1",
+    name: "Nike for Mens (Blue & Orange) ",
+    image: "dfs",
+    description: "Brand New NIKE shoes for Mens ",
+    price: "$99",
+    countInStock: 5,
+    rating: 4.5 / 5,
+    numReviews: 20,
+  },
 
+  {
+    _id: "2",
+    name: "Adidas for Mens (White) ",
+    image: URL("./2.jpeg"),
+    description: "Brand New ADIDAS shoes for Mens ",
+    price: "$89",
+    countInStock: 5,
+    rating: 4 / 5,
+    numReviews: 10,
+  },
 
-    {
-        _id : "2",
-        name :"Adidas for Mens (White) ",
-        image:"./2.jpeg",
-        description:"Brand New ADIDAS shoes for Mens ",
-        price:"$89",
-        countInStock:5,
-        rating:4/5,
-        numReviews:10,
-    },
+  {
+    _id: "3",
+    name: "Nike for Mens (yellow) ",
+    image: "./3.jpeg",
+    description: "Brand New NIKE shoes for Mens ",
+    price: "$99",
+    countInStock: 5,
+    rating: 4.5 / 5,
+    numReviews: 20,
+  },
 
+  {
+    _id: "4",
+    name: "Puma for Mens (White & Black) ",
+    image: "./4.jpeg",
+    description: "Brand New PUMA shoes for Mens ",
+    price: "$79",
+    countInStock: 4,
+    rating: 4.5 / 5,
+    numReviews: 15,
+  },
 
-    {
-        _id : "3",
-        name :"Nike for Mens (yellow) ",
-        image:"./3.jpeg",
-        description:"Brand New NIKE shoes for Mens ",
-        price:"$99",
-        countInStock:5,
-        rating:4.5/5,
-        numReviews:20,
-    },
+  {
+    _id: "5",
+    name: "WoodLand for Mens  ",
+    image: "./4.jpeg",
+    description: "Brand New WOODLAND shoes for Mens ",
+    price: "$149",
+    countInStock: 8,
+    rating: 4.5 / 5,
+    numReviews: 15,
+  },
 
-
-    {
-        _id : "4",
-        name :"Puma for Mens (White & Black) ",
-        image:"./4.jpeg",
-        description:"Brand New PUMA shoes for Mens ",
-        price:"$79",
-        countInStock:4,
-        rating:4.5/5,
-        numReviews:15,
-    },
-
-
-    {
-        _id : "5",
-        name :"WoodLand for Mens  ",
-        image:"./4.jpeg",
-        description:"Brand New WOODLAND shoes for Mens ",
-        price:"$149",
-        countInStock:8,
-        rating:4.5/5,
-        numReviews:15,
-    },
-
-
-    {
-        _id : "6",
-        name :"Bata for Mens ( sky-blue) ",
-        image:"./4.jpeg",
-        description:"Brand New BATA shoes for Mens ",
-        price:"$59",
-        countInStock:10,
-        rating:4.5/5,
-        numReviews:15,
-    },
-    
-
+  {
+    _id: "6",
+    name: "Bata for Mens ( sky-blue) ",
+    image: "./4.jpeg",
+    description: "Brand New BATA shoes for Mens ",
+    price: "$59",
+    countInStock: 10,
+    rating: 4.5 / 5,
+    numReviews: 15,
+  },
 ]);
-export default pro
+export default pro;
